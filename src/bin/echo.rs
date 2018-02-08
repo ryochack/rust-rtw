d9344a09b6720f50993fead77fed9639c2443964
ryochack <ryochack@gmail.com>
2018-02-09 04:46:54 +0900

    echo: support options

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -1,8 +1,36 @@
 use std::env;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    for (i, v) in args[1..].iter().enumerate() {
+fn unescape(s: &[String]) {
+    for (i, v) in s.iter().enumerate() {
+        if i > 0 {
+            print!(" ");
+        }
+        let mut backslashed = false;
+        for c in v.chars() {
+            if c == '\\' && !backslashed {
+                backslashed = true;
+                continue;
+            }
+            if backslashed {
+                match c {
+                    'n' => print!("\n"),
+                    'r' => print!("\r"),
+                    't' => print!("\t"),
+                    _ => print!("\\{}", c),
+                }
+                backslashed = false;
+            } else {
+                print!("{}", c);
+            }
+        }
+        if backslashed {
+            print!("\\");
+        }
+    }
+}
+
+fn raw(s: &[String]) {
+    for (i, v) in s.iter().enumerate() {
         print!("{}{}",
                if i == 0 {
                    ""
@@ -10,5 +38,33 @@ fn main() {
                    " "
                }, v);
     }
-    println!()
+}
+
+fn main() {
+    let mut output_trailing_newline = true;
+    let mut enable_interp_backslash_escapes = true;
+
+    let args: Vec<String> = env::args().skip(1).collect();
+    let mut s = args.as_slice();
+
+    for (i, v) in s.iter().enumerate() {
+        match v.as_str() {
+            "-e" => enable_interp_backslash_escapes = true,
+            "-E" => enable_interp_backslash_escapes = false,
+            "-n" => output_trailing_newline = false,
+            _ => {
+                s = &s[i..];
+                break;
+            }
+        }
+    }
+
+    if enable_interp_backslash_escapes {
+        unescape(&s);
+    } else {
+        raw(&s);
+    }
+    if output_trailing_newline {
+        println!();
+    }
 }
